a7856714585552b2724ee60dc5e33187a36b885b
Felix <felixen67@gmail.com>
2026-07-13 23:16:22 +0200

    Add usage text + basic arg parsing

--- main.c
+++ main.c
@@ -1,11 +1,33 @@
 #include "player.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-int main(void) {
-    srand(time(NULL));
+static const char help[] = "Usage: hockeymgr <options>\n"
+                           "-h, --help\n"
+                           "\tPrint this message.\n"
+                           "-t, --test\n"
+                           "\tRun all tests.\n";
+
+static void test_random_player(void) {
     Player_t *random_player = create_random_player(POS_LD);
     print_player(random_player);
+}
+
+int main(int argc, char **argv) {
+    if (argc == 2) {
+        if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+            printf("%s", help);
+            return 0;
+        }
+        if (!strcmp(argv[1], "-t") || !strcmp(argv[1], "--help")) {
+            /* Place all tests here. */
+            test_random_player();
+            return 0;
+        }
+    }
+    srand(time(NULL));
     return 0;
 }
 
